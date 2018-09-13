806115379aec236de9f1223210e6b3a7d3147141
Danilo <danilojesusv@gmail.com>
2018-09-13 15:48:19 -0500

    video react

--- src/banner/components/title.js
+++ src/banner/components/title.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import './title.css';
+import Video from './video.js';
 
 class Title extends Component {
-	render() {
 
-		const styles = {
+	state = {
+		title: 'Danilo Viacava'
+	}
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			title: props.title,
+		}
+	}
+
+	handleClick = (event) => {
+		//console.log(this.props.image);
+		this.setState({
+			title: 'Ricardo Celis',
+		})
+	}
+
+	render() {
+		/*const styles = {
 			container: {
 				fontSize: '20px',
-				background: 'red',
+				background: 'transparent',
 				fontWeight: 'bold',
 				fontFamily: 'cursive',
-				width: 300,
+				width: 'auto',
 			},
 			img: {
 				maxWidth: '100%',
@@ -18,20 +37,22 @@ class Title extends Component {
 				display: 'block',
 				width: '100%',
 			}
-		}
+		}*/
 
 		return (
-			<div style={styles.container}>
-				<div className="Title">
-					<p className="TitleMain">Hello Title</p>
+			<div className="Relative">
+				<Video />
+				<div className="TitleAbsolute">
+					<p className="TitleMain">{this.state.title}</p>
 					<img 
-						style={styles.img}
-						src="https://camo.githubusercontent.com/9c8486f1f9e23ad3e14e118bedc909cbc92ec937/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f3331343939372f313535333237362f31313733373165362d346533312d313165332d393539632d3636613061303932376464352e706e67"
+						className="TitleImage"
+						src={this.props.image}
 						alt=""
 						width={"auto"}
 						height={"auto"}
 					/>
 					<h3>Subtitle</h3>
+					<button className="ButtonBanner" onClick={this.handleClick}>Presioname</button>
 				</div>
 			</div>
 		)
---
+++ src/banner/components/video.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Player } from 'video-react';
+import './video-react.css'
+import './video.css'
+
+export default (props) => {
+  return (
+    <Player
+      autoPlay
+      playsInline
+      poster=""
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      //src="/src/banner/components/DJI_0014.MOV"
+    />
+  )
+}
